ibmruntimes/v8ppc

commit 6c30630405e1729d1c69880c3add849f4bdab530

Fix ThrowUncatchable - incorrect test logic. Use better sequence on out of memory check.

src/ppc/macro-assembler-ppc.cc
@@ -1485,10 +1485,8 @@ void MacroAssembler::ThrowUncatchable(Register value) {
 
   bind(&check_kind);
   STATIC_ASSERT(StackHandler::JS_ENTRY == 0);
-  lwz(r5, MemOperand(sp, StackHandlerConstants::kStateOffset));
-  mov(r0, Operand(StackHandler::KindField::kMask));
-  cmp(r5, r0);
-  bne(&fetch_next);
+  andi(r0, r5, Operand(StackHandler::KindField::kMask));
+  bne(&fetch_next, cr0);
 
   // Set the top handler address to next handler past the top ENTRY handler.
   pop(r5);

src/ppc/code-stubs-ppc.cc
@@ -4061,8 +4061,7 @@ void CEntryStub::GenerateCore(MacroAssembler* masm,
 
   // Special handling of out of memory exceptions.
   Failure* out_of_memory = Failure::OutOfMemoryException();
-  __ mov(r6, Operand(reinterpret_cast<int32_t>(out_of_memory)));
-  __ cmp(r3, r6);
+  __ cmpi(r3, Operand(reinterpret_cast<int32_t>(out_of_memory)));
   __ beq(throw_out_of_memory_exception);
 
   // Retrieve the pending exception and clear the variable.

commit 6137cb96c61e0f6b9892872c90f0318089f0c121

Fix MacroAssembler::ThrowUncatchable
It seems that commit 6c306304 inadvertently removed a load instr.

src/ppc/macro-assembler-ppc.cc
@@ -1289,6 +1289,7 @@ void MacroAssembler::ThrowUncatchable(Register value) {
 
   bind(&check_kind);
   STATIC_ASSERT(StackHandler::JS_ENTRY == 0);
+  lwz(r5, MemOperand(sp, StackHandlerConstants::kStateOffset));
   andi(r0, r5, Operand(StackHandler::KindField::kMask));
   bne(&fetch_next, cr0);
 