ibmruntimes/v8ppc

commit 36e15092e96949e1f5c1d558479102fb992815c4

Fix Smi multiplication for 64-bit.

src/ppc/simulator-ppc.cc
@@ -1916,8 +1916,8 @@ void Simulator::DecodeExt2_9bit(Instruction* instr) {
       int rt = instr->RTValue();
       int ra = instr->RAValue();
       int rb = instr->RBValue();
-      int64_t ra_val = get_register(ra);
-      int64_t rb_val = get_register(rb);
+      int32_t ra_val = (get_register(ra) & 0xFFFFFFFF);
+      int32_t rb_val = (get_register(rb) & 0xFFFFFFFF);
       int64_t alu_out = ra_val * rb_val;
       alu_out >>= 32;
       set_register(rt, alu_out);
@@ -2108,9 +2108,9 @@ void Simulator::DecodeExt2_9bit(Instruction* instr) {
       int rt = instr->RTValue();
       int ra = instr->RAValue();
       int rb = instr->RBValue();
-      intptr_t ra_val = get_register(ra);
-      intptr_t rb_val = get_register(rb);
-      intptr_t alu_out = ra_val * rb_val;
+      int32_t ra_val = (get_register(ra) & 0xFFFFFFFF);
+      int32_t rb_val = (get_register(rb) & 0xFFFFFFFF);
+      int32_t alu_out = ra_val * rb_val;
       set_register(rt, alu_out);
       if (instr->Bit(0)) {  // RC bit set
         SetCR0(alu_out);

src/ppc/code-stubs-ppc.cc
@@ -2120,14 +2120,23 @@ void BinaryOpStub::GenerateSmiSmiOperation(MacroAssembler* masm) {
     }
     case Token::MUL: {
       Label mul_zero, mul_neg_zero;
+#if V8_TARGET_ARCH_PPC64
+      // Remove tag from both operands.
+      __ SmiUntag(ip, right);
+      __ SmiUntag(r0, left);
+      // Do multiplication
+      // scratch1 = lower 32 bits of product.
+      // scratch2 = higher 32 bits of product.
+      __ mullw(scratch1, r0, ip);
+      __ mulhw(scratch2, r0, ip);
+#else
       // Remove tag from one of the operands. This way the multiplication result
       // will be a smi if it fits the smi range.
       __ SmiUntag(ip, right);
       // Do multiplication
-      // scratch1 = lower 32 bits of ip * left.
-      // scratch2 = higher 32 bits of ip * left.
       __ mullw(scratch1, left, ip);
       __ mulhw(scratch2, left, ip);
+#endif
       // Check for overflowing the smi range - no overflow if higher 33 bits of
       // the result are identical.
       __ srawi(ip, scratch1, 31);
@@ -2136,7 +2145,11 @@ void BinaryOpStub::GenerateSmiSmiOperation(MacroAssembler* masm) {
       // Go slow on zero result to handle -0.
       __ cmpi(scratch1, Operand::Zero());
       __ beq(&mul_zero);
+#if V8_TARGET_ARCH_PPC64
+      __ SmiTag(right, scratch1);
+#else
       __ mr(right, scratch1);
+#endif
       __ Ret();
       __ bind(&mul_zero);
       // We need -0 if we were multiplying a negative number with 0 to get 0.

src/ppc/full-codegen-ppc.cc
@@ -2065,16 +2065,28 @@ void FullCodeGenerator::EmitInlineSmiBinaryOp(BinaryOperation* expr,
       break;
     }
     case Token::MUL: {
+#if V8_TARGET_ARCH_PPC64
+      // Remove tag from both operands.
+      __ SmiUntag(ip, right);
+      __ SmiUntag(r0, left);
+      __ mullw(scratch1, r0, ip);
+      __ mulhw(scratch2, r0, ip);
+#else
       __ SmiUntag(ip, right);
       __ mullw(scratch1, left, ip);
       __ mulhw(scratch2, left, ip);
+#endif
       // Check for overflowing the smi range - no overflow if higher 33 bits of
       // the result are identical.
       __ srawi(ip, scratch1, 31);
       __ cmp(ip, scratch2);
       __ bne(&stub_call);
       // Go slow on zero result to handle -0.
+#if V8_TARGET_ARCH_PPC64
+      __ SmiTag(right, scratch1);
+#else
       __ mr(right, scratch1);
+#endif
       __ cmpi(scratch1, Operand::Zero());
       __ bne(&done);
       // We need -0 if we were multiplying a negative number with 0 to get 0.
@@ -3802,10 +3814,14 @@ void FullCodeGenerator::EmitFastAsciiArrayJoin(CallRuntime* expr) {
   __ JumpIfInstanceTypeIsNotSequentialAscii(scratch1, scratch2, &bailout);
 
   // Add (separator length times array_length) - separator length to the
-  // string_length to get the length of the result string. array_length is not
-  // smi but the other values are, so the result is a smi
+  // string_length to get the length of the result string.
   __ LoadP(scratch1, FieldMemOperand(separator, SeqAsciiString::kLengthOffset));
   __ sub(string_length, string_length, scratch1);
+#if V8_TARGET_ARCH_PPC64
+  __ SmiUntag(scratch1, scratch1);
+#else
+  // array_length is not smi but the other values are, so the result is a smi
+#endif
   __ mullw(scratch2, array_length, scratch1);
   __ mulhw(ip, array_length, scratch1);
   // Check for smi overflow. No overflow if higher 33 bits of 64-bit result are
@@ -3815,6 +3831,9 @@ void FullCodeGenerator::EmitFastAsciiArrayJoin(CallRuntime* expr) {
   __ TestSignBit(scratch2, r0);
   __ bne(&bailout, cr0);
 
+#if V8_TARGET_ARCH_PPC64
+  __ SmiTag(scratch2, scratch2);
+#endif
   __ AddAndCheckForOverflow(string_length, string_length, scratch2,
                             scratch1, r0);
   __ BranchOnOverflow(&bailout);

commit a821018591edccf8ed4de4d76154ef0f3bc0ddd5

Fix bug in mulhw simulation introduced by 36e1509

src/ppc/simulator-ppc.cc
@@ -1916,8 +1916,8 @@ void Simulator::DecodeExt2_9bit(Instruction* instr) {
       int rt = instr->RTValue();
       int ra = instr->RAValue();
       int rb = instr->RBValue();
-      int32_t ra_val = (get_register(ra) & 0xFFFFFFFF);
-      int32_t rb_val = (get_register(rb) & 0xFFFFFFFF);
+      int64_t ra_val = (get_register(ra) & 0xFFFFFFFF);
+      int64_t rb_val = (get_register(rb) & 0xFFFFFFFF);
       int64_t alu_out = ra_val * rb_val;
       alu_out >>= 32;
       set_register(rt, alu_out);