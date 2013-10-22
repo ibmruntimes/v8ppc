ibmruntimes/v8ppc

commit 2a5cd57e047106fe144fd5b5177e1227463b6025

Fix compatibility with G4/G5 processors These pre-POWER5 processors do not support the frim instruction.
Added processor detection (for Linux only at present)
and use an alternative instructions sequence without
frim for G4 and G5 processors.

src/ppc/assembler-ppc.cc
@@ -60,6 +60,48 @@ static unsigned CpuFeaturesImpliedByCompiler() {
   return answer;
 }
 
+#if !defined(_AIX)
+// This function uses types in elf.h
+static const char *read_cpu_type() {
+  char *result = NULL;
+  // Open the AUXV (auxilliary vector) psuedo-file
+  int fd = open("/proc/self/auxv", O_RDONLY);
+
+  if (fd != -1) {
+#if V8_TARGET_ARCH_PPC64
+    Elf64_auxv_t buffer[16];
+    Elf64_auxv_t *auxv_element;
+#else
+    Elf32_auxv_t buffer[16];
+    Elf32_auxv_t *auxv_element;
+#endif
+    int bytes_read = 0;
+    while (bytes_read >= 0) {
+      // Read a chunk of the AUXV
+      bytes_read = read(fd, buffer, sizeof(buffer));
+      // Locate and read the platform field of AUXV if it is in the chunk
+      for (auxv_element = buffer; auxv_element < buffer+bytes_read && auxv_element->a_type != AT_NULL; auxv_element++) {
+        if (auxv_element->a_type == AT_PLATFORM) {
+          result = (char *)auxv_element->a_un.a_val;
+          goto done_reading;
+        }
+      }
+    }
+    done_reading:
+    close(fd);
+  }
+
+  return result;
+}
+
+static bool is_processor(const char* p) {
+  static const char *auxv_cpu_type = read_cpu_type();
+  if (auxv_cpu_type == NULL) { PrintF("cpu_type = null\n"); return false; }
+  // PrintF("cpu_type = %s\n", auxv_cpu_type);
+  return (strcmp(auxv_cpu_type, p) == 0);
+}
+#endif
+
 void CpuFeatures::Probe() {
   unsigned standard_features = static_cast<unsigned>(
       OS::CpuFeaturesImpliedByPlatform()) | CpuFeaturesImpliedByCompiler();
@@ -77,6 +119,20 @@ void CpuFeatures::Probe() {
     // No probing for features if we might serialize (generate snapshot).
     return;
   }
+
+  // Detect whether frim instruction is supported (POWER5+)
+  // For now we will just check for processors we know do not
+  // support it
+#if !defined(_AIX)
+  if (!is_processor("ppc970") /* G5 */ && !is_processor("ppc7450") /* G4 */) {
+    // Assume support
+    supported_ |= (1u << FPU);
+  }
+#else
+  // Fallback: assume frim is supported -- will implement processor
+  // detection for other PPC platforms in is_processor() if required
+  supported_ |= (1u << FPU);
+#endif
 }
 
 Register ToRegister(int num) {

src/ppc/stub-cache-ppc.cc
@@ -2139,13 +2139,29 @@ Handle<Code> CallStubCompiler::CompileMathFloorCall(
   __ lfd(d1, FieldMemOperand(r3, HeapNumber::kValueOffset));
 
   // Round to integer minus
-  __ frim(d1, d1);
-  // Convert the argument to an integer.
+  if (CpuFeatures::IsSupported(FPU)) {
+    // The frim instruction is only supported on POWER5 
+    // and higher
+    __ frim(d1, d1);
 #if V8_TARGET_ARCH_PPC64
-  __ fctidz(d1, d1);
+    __ fctidz(d1, d1);
 #else
-  __ fctiwz(d1, d1);
+    __ fctiwz(d1, d1);
 #endif
+  } else {
+    // This sequence is more portable (avoids frim)
+    // This should be evaluated to determine if frim provides any 
+    // perf benefit or if we can simply use the compatible sequence 
+    // always
+    __ SetRoundingMode(kRoundToMinusInf);
+#if V8_TARGET_ARCH_PPC64
+    __ fctid(d1, d1);
+#else
+    __ fctiw(d1, d1);
+#endif
+    __ ResetRoundingMode();
+  }
+  // Convert the argument to an integer.
   __ stfdu(d1, MemOperand(sp, -8));
 #if V8_TARGET_ARCH_PPC64
   __ ld(r3, MemOperand(sp, 0));

src/ppc/assembler-ppc.h
@@ -44,6 +44,9 @@
 #ifndef V8_PPC_ASSEMBLER_PPC_H_
 #define V8_PPC_ASSEMBLER_PPC_H_
 #include <stdio.h>
+#include <elf.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "assembler.h"
 #include "constants-ppc.h"
 #include "serialize.h"

commit 327b4a165c42c70e7d12d619039c844a2ca2d907

Fixup presubmit errors and violations in recent commit Introduced in commit 2a5cd57e047106fe144fd5b5177e1227463b6025.

src/ppc/assembler-ppc.cc
@@ -80,9 +80,11 @@ static const char *read_cpu_type() {
       // Read a chunk of the AUXV
       bytes_read = read(fd, buffer, sizeof(buffer));
       // Locate and read the platform field of AUXV if it is in the chunk
-      for (auxv_element = buffer; auxv_element < buffer+bytes_read && auxv_element->a_type != AT_NULL; auxv_element++) {
+      for (auxv_element = buffer;
+           auxv_element < buffer+bytes_read && auxv_element->a_type != AT_NULL;
+           auxv_element++) {
         if (auxv_element->a_type == AT_PLATFORM) {
-          result = (char *)auxv_element->a_un.a_val;
+          result = reinterpret_cast<char*>(auxv_element->a_un.a_val);
           goto done_reading;
         }
       }

src/ppc/stub-cache-ppc.cc
@@ -2140,7 +2140,7 @@ Handle<Code> CallStubCompiler::CompileMathFloorCall(
 
   // Round to integer minus
   if (CpuFeatures::IsSupported(FPU)) {
-    // The frim instruction is only supported on POWER5 
+    // The frim instruction is only supported on POWER5
     // and higher
     __ frim(d1, d1);
 #if V8_TARGET_ARCH_PPC64
@@ -2150,8 +2150,8 @@ Handle<Code> CallStubCompiler::CompileMathFloorCall(
 #endif
   } else {
     // This sequence is more portable (avoids frim)
-    // This should be evaluated to determine if frim provides any 
-    // perf benefit or if we can simply use the compatible sequence 
+    // This should be evaluated to determine if frim provides any
+    // perf benefit or if we can simply use the compatible sequence
     // always
     __ SetRoundingMode(kRoundToMinusInf);
 #if V8_TARGET_ARCH_PPC64