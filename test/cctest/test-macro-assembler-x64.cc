c64c6721e72a7f13d4b32255288b3b0f10aaa2d8
Andrew Low <Andrew_Low@ca.ibm.com>
2014-02-03 15:27:42 -0500

    This reverts commit 79cdf70 And merges in the following change from upstream https://code.google.com/p/v8/source/detail?r=17641

--- test/cctest/test-macro-assembler-x64.cc
+++ test/cctest/test-macro-assembler-x64.cc
@@ -2280,8 +2280,8 @@ TEST(PositiveSmiTimesPowerOfTwoToInteger64) {
 
 TEST(OperandOffset) {
   v8::internal::V8::Initialize(NULL);
-  int data[256];
-  for (int i = 0; i < 256; i++) { data[i] = i * 0x01010101; }
+  uint32_t data[256];
+  for (uint32_t i = 0; i < 256; i++) { data[i] = i * 0x01010101; }
 
   // Allocate an executable page of memory.
   size_t actual_size;
