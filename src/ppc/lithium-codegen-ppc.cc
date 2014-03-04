5a5ec0841735083c6ac5db7c7fe3f194768d475a
Matthew Brandyberry <mbrandy@us.ibm.com>
2014-03-04 16:00:56 -0600

    Implement 3.22.24.5 patch for PPC.

--- src/ppc/lithium-codegen-ppc.cc
+++ src/ppc/lithium-codegen-ppc.cc
@@ -5746,6 +5746,11 @@ void LCodeGen::DoDeoptimize(LDeoptimize* instr) {
 }
 
 
+void LCodeGen::DoDummy(LDummy* instr) {
+  // Nothing to see here, move on!
+}
+
+
 void LCodeGen::DoDummyUse(LDummyUse* instr) {
   // Nothing to see here, move on!
 }
