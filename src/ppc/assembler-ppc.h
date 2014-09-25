1945baf5c5afcfa6599dda2425ec6fafe6e7f7db
Matthew Brandyberry <mbrandy@us.ibm.com>
2014-09-25 14:09:40 -0500

    PPC: perf enhancement: use bctrl for calls (vs blrl).

--- src/ppc/assembler-ppc.h
+++ src/ppc/assembler-ppc.h
@@ -778,6 +778,7 @@ class Assembler : public AssemblerBase {
 
   void bcctr(BOfield bo, LKBit lk);
   void bctr();
+  void bctrl();
 
   // Convenience branch instructions using labels
   void b(Label* L, LKBit lk = LeaveLK)  {
