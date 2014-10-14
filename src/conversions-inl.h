b0dcfc36ff38349d6829e209f2874d400cc675b4
Matthew Brandyberry <mbrandy@us.ibm.com>
2014-10-14 14:04:49 -0500

    AIX: Fix DoubleToInteger.
    AIX std::ceil() does not return negative zero.

--- src/conversions-inl.h
+++ src/conversions-inl.h
@@ -69,7 +69,12 @@ inline unsigned int FastD2UI(double x) {
 inline double DoubleToInteger(double x) {
   if (std::isnan(x)) return 0;
   if (!std::isfinite(x) || x == 0) return x;
+#if V8_OS_AIX
+  // AIX ceil does not return negative zero.
+  return (x >= 0) ? std::floor(x) : -std::floor(-x);
+#else
   return (x >= 0) ? std::floor(x) : std::ceil(x);
+#endif
 }
 
 
