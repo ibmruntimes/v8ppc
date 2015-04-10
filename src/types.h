9aca3bfb8e700ca638f269a0ad93a384ffb5a374
Matthew Brandyberry <mbrandy@us.ibm.com>
2015-04-10 10:08:02 -0500

    Fix type definitions under V8_PPC_TAGGING_OPT after merge.

--- src/types.h
+++ src/types.h
@@ -253,7 +253,7 @@ namespace internal {
   V(InternalizedString,  1u << 12 | REPRESENTATION(kTaggedPointer)) \
   V(OtherString,         1u << 13 | REPRESENTATION(kTaggedPointer)) \
   V(Undetectable,        1u << 14 | REPRESENTATION(kTaggedPointer)) \
-  V(Array,               1u << 15 | REPRESENTATION(kTaggedPointer)) \
+  V(GlobalObject,        1u << 15 | REPRESENTATION(kTaggedPointer)) \
   V(OtherObject,         1u << 16 | REPRESENTATION(kTaggedPointer)) \
   V(Proxy,               1u << 17 | REPRESENTATION(kTaggedPointer)) \
   V(Internal,            1u << 18 | REPRESENTATION(kTagged | kUntagged)) \
@@ -273,7 +273,7 @@ namespace internal {
   V(NumberOrString,      kNumber | kString) \
   V(PlainPrimitive,      kNumberOrString | kBoolean | kNull | kUndefined) \
   V(Primitive,           kSymbol | kPlainPrimitive) \
-  V(DetectableObject,    kArray | kOtherObject) \
+  V(DetectableObject,    kGlobalObject | kOtherObject) \
   V(DetectableReceiver,  kDetectableObject | kProxy) \
   V(Detectable,          kDetectableReceiver | kNumber | kName) \
   V(Object,              kDetectableObject | kUndetectable) \
