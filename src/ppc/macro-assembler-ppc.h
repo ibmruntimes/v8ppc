5254500d79036248455be247b9f8f19cb6c005a0
Donald Stence <dstence@us.ibm.com>
2015-06-10 15:51:21 -0400

    testSMI optimization

--- src/ppc/macro-assembler-ppc.h
+++ src/ppc/macro-assembler-ppc.h
@@ -1064,7 +1064,11 @@ class MacroAssembler : public Assembler {
     int rotate = (rangeEnd == 0) ? 0 : kBitsPerPointer - rangeEnd;
     int width = rangeStart - rangeEnd + 1;
 #if V8_TARGET_ARCH_PPC64
-    rldicl(dst, src, rotate, kBitsPerPointer - width, rc);
+    if (rc == SetRC && rangeEnd == 0 && width <= 16) {
+      andi(dst, src, Operand((1 << (rangeStart+1))-1));
+    } else {
+      rldicl(dst, src, rotate, kBitsPerPointer - width, rc);
+    }
 #else
     rlwinm(dst, src, rotate, kBitsPerPointer - width, kBitsPerPointer - 1, rc);
 #endif
