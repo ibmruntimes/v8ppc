d131ac058c17fdfd8e1bbfd5e8229372477dbd4e
Matthew Brandyberry <mbrandy@us.ibm.com>
2016-01-13 10:35:32 -0600

    PPC: [builtins] Sanitize receiver patching for API functions.
    Port 12bcba1543235c3e0fc02b11aecf7fc16fdf06a8
    
    Original commit message:
        The API functions are always in sloppy mode, so receiver is always a
        JSReceiver once the actual call trampoline runs, no need to check again
        in various places.
    
    R=bmeurer@chromium.org

--- src/ppc/builtins-ppc.cc
+++ src/ppc/builtins-ppc.cc
@@ -1211,9 +1211,6 @@ void CompatibleReceiverCheck(MacroAssembler* masm, Register receiver,
   Register constructor = r9;
   Register scratch = r10;
 
-  __ CompareObjectType(receiver, map, no_reg, FIRST_JS_OBJECT_TYPE);
-  __ blt(receiver_check_failed);
-
   // If there is no signature, return the holder.
   __ LoadP(signature, FieldMemOperand(function_template_info,
                                       FunctionTemplateInfo::kSignatureOffset));
@@ -1283,21 +1280,15 @@ void Builtins::Generate_HandleFastApiCall(MacroAssembler* masm) {
   //  -- sp[4 * argc]       : receiver
   // -----------------------------------
 
-  // Load the receiver.
-  __ ShiftLeftImm(r11, r3, Operand(kPointerSizeLog2));
-  __ LoadPX(r5, MemOperand(sp, r11));
-
-  // Update the receiver if this is a contextual call.
-  Label set_global_proxy, valid_receiver;
-  __ JumpIfRoot(r5, Heap::kUndefinedValueRootIndex, &set_global_proxy);
 
   // Load the FunctionTemplateInfo.
-  __ bind(&valid_receiver);
   __ LoadP(r6, FieldMemOperand(r4, JSFunction::kSharedFunctionInfoOffset));
   __ LoadP(r6, FieldMemOperand(r6, SharedFunctionInfo::kFunctionDataOffset));
 
   // Do the compatible receiver check.
   Label receiver_check_failed;
+  __ ShiftLeftImm(r11, r3, Operand(kPointerSizeLog2));
+  __ LoadPX(r5, MemOperand(sp, r11));
   CompatibleReceiverCheck(masm, r5, r6, &receiver_check_failed);
 
   // Get the callback offset from the FunctionTemplateInfo, and jump to the
@@ -1306,11 +1297,6 @@ void Builtins::Generate_HandleFastApiCall(MacroAssembler* masm) {
   __ LoadP(r7, FieldMemOperand(r7, CallHandlerInfo::kFastHandlerOffset));
   __ addi(ip, r7, Operand(Code::kHeaderSize - kHeapObjectTag));
   __ JumpToJSEntry(ip);
-
-  __ bind(&set_global_proxy);
-  __ LoadGlobalProxy(r5);
-  __ StorePX(r5, MemOperand(sp, r11));
-  __ b(&valid_receiver);
 
   // Compatible receiver check failed: throw an Illegal Invocation exception.
   __ bind(&receiver_check_failed);
